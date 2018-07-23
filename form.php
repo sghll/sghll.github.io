2b60f4a7314d07645475a476a6f76303b2fac083
sghll <40046073+sghll@users.noreply.github.com>
2018-07-24 00:09:40 +1000

    contact form adjust

---
+++ form.php
@@ -0,0 +1,17 @@
+<div class="contact-form">
+  <form id="qcf" action="" method="post">
+    <h3>Quick Contact</h3>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" tabindex="2">
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+</div>
