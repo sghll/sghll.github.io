dd4ccdc97395b76f8bc050cfbc3d459c34d93abf
sghll <40046073+sghll@users.noreply.github.com>
2018-07-24 18:49:18 +1000

    php test

--- form_process.php
+++ form_process.php
@@ -1,31 +1,31 @@
 <?php
-
-//form is submitted with POST method
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  if (empty($_POST["name"])) {
-    $name = "";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
-
-  if (empty($_POST["email"])) {
-    $email = "";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
-
-  if (empty($_POST["message"]) == false) {
-
-    $message = test_input($_POST["message"]);
-    
-    $message_body = '';
-    $to = 'sghll@hotmail.com';
-    $subject = 'Contact Form Submit';
-    mail($to, $subject, $message);
-
-  }
-
-}
+echo "Hello";
 
 ?>
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//
+//   if (empty($_POST["name"])) {
+//     $name = "";
+//   } else {
+//     $name = test_input($_POST["name"]);
+//   }
+//
+//   if (empty($_POST["email"])) {
+//     $email = "";
+//   } else {
+//     $email = test_input($_POST["email"]);
+//   }
+//
+//   if (empty($_POST["message"]) == false) {
+//
+//     $message = test_input($_POST["message"]);
+//
+//     $message_body = '';
+//     $to = 'sghll@hotmail.com';
+//     $subject = 'Contact Form Submit';
+//     mail($to, $subject, $message);
+//
+//   }
+//
+// }
