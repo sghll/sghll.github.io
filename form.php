02bde6e228ffcb0398ce9df58df7de11fabed06e
sghll <40046073+sghll@users.noreply.github.com>
2018-07-24 14:43:55 +1000

    create php file

--- form.php
+++ form.php
@@ -1,17 +1,15 @@
-<div class="contact-form">
-  <form id="qcf" action="" method="post">
-    <h3>Quick Contact</h3>
-    <fieldset>
-      <input placeholder="Your name" type="text" tabindex="1" autofocus>
-    </fieldset>
-    <fieldset>
-      <input placeholder="Your Email Address" type="email" tabindex="2">
-    </fieldset>
-    <fieldset>
-      <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
-    </fieldset>
-    <fieldset>
-      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-    </fieldset>
-  </form>
-</div>
+<form id="qcf" action="" method="post">
+  <h3>Quick Contact</h3>
+  <fieldset>
+    <input placeholder="Your name" type="text" tabindex="1" autofocus>
+  </fieldset>
+  <fieldset>
+    <input placeholder="Your Email Address" type="text" tabindex="2">
+  </fieldset>
+  <fieldset>
+    <textarea placeholder="Type your Message Here...." type="text" tabindex="3"></textarea>
+  </fieldset>
+  <fieldset>
+    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+  </fieldset>
+</form>
