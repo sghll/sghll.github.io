939333a2108f77c28f5443615a98130dee81aabc
sghll <40046073+sghll@users.noreply.github.com>
2018-07-25 14:00:21 +1000

    final adjust

--- form_process.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?><br>
-    And you wrote this: <?php echo "message"; ?>
-
-  </body>
-</html>
